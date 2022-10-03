7c7eab59d48c9203ff9f3fdbc6b01da2caf87cbb
weichweich <albrecht@kilt.io>
2022-10-03 14:55:52 +0200

    fix again

--- src/calls.rs
+++ src/calls.rs
@@ -13,17 +13,17 @@ use subxt::tx::TxPayload;
 
 /// Proposal hash for the referendum (parachain_system > authorize_code("0x1c46dd62730a80d3da9d43bd544ca30e32a3654a58edc9df0517249b5708b6c1"))
 const REF_PROPOSAL_HASH: H256 = H256(hex!(
-    "fb8de2b39e7d3ea0ec67a8d4af356cb3e988ebae72a8f1eb2afcc1b85f20df34"
+    "8dbbae9bc550d1f993abf27ac4fbf265bf6da0e675b42ab0124ada7ef3613f3a"
 ));
 /// The call bytes that should be executed parachain_system > authorize_code("0x1c46dd62730a80d3da9d43bd544ca30e32a3654a58edc9df0517249b5708b6c1")
 const REF_PROPOSAL_CALL: &[u8] =
-    &hex!("5004801c46dd62730a80d3da9d43bd544ca30e32a3654a58edc9df0517249b5708b6c1");
+    &hex!("50031c46dd62730a80d3da9d43bd544ca30e32a3654a58edc9df0517249b5708b6c1");
 
 const COUNCIL_PROPOSAL_HASH: H256 = H256(hex!(
-    "761a7cf0242c8d6ba247df76aa41ee75db5f4143b8de503858a152ef1276fe05"
+    "4082d04b7cd87bce6f9af1c4430851c4f3952090f627615a51bae449dd5620ed"
 ));
 const TC_PROPOSAL_HASH: H256 = H256(hex!(
-    "0c138353532284516427bf7ba2819874e542e6fbbe39325d17c3e63415b33ebf"
+    "43567cece6190549eb2ea81800981e451c0dd23abe9acdc0501fb1ff1adbae43"
 ));
 const PROPOSAL_WEIGHT: u64 = 900_000_000;
 const PROPOSAL_LENGTH: u32 = 100;
